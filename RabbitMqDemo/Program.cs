723c66644b4d2baef0ef630aca3c2ad8358d0153
Keamo25 <62904137+Keamo25@users.noreply.github.com>
2021-07-05 00:57:46 +0200

    Add project files.

---
+++ RabbitMqDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitMqDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name below:");
+            string message = Console.ReadLine();
+            try
+            {
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "parentQueue",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "parentQueue",
+                                         basicProperties: null,
+                                         body: body);
+                    Console.WriteLine($"Hello my name is, {message}");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+    }
+}
---
+++ Receiver/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Receiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "parentQueue",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var ReceivedName = Encoding.UTF8.GetString(body);
+                    Console.WriteLine($"Hello {ReceivedName}, I am your father!”");
+
+                };
+                channel.BasicConsume(queue: "parentQueue",
+                                     autoAck: true,
+                                     consumer: consumer);
+
+            }
+        }
+    }
+}
